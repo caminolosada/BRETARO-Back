568a792af90d174b7931df9cfc6713fe0162f83c
Camino Losada <camino.losada.alonso@gmail.com>
2023-05-25 17:34:50 +0200

    Add test cases for generalError middleware

--- src/server/middleware/errorMiddleware.test.ts
+++ src/server/middleware/errorMiddleware.test.ts
@@ -1,6 +1,10 @@
 import { type NextFunction, type Request, type Response } from "express";
-import { notFoundError } from "./errorMiddleware";
+import { generalError, notFoundError } from "./errorMiddleware";
 import CustomError from "../CustomError/CustomError";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 describe("Given a notFoundError middleware", () => {
   describe("When it receives a next function", () => {
@@ -14,6 +18,64 @@ describe("Given a notFoundError middleware", () => {
       notFoundError(req as Request, res as Response, next as NextFunction);
 
       expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+
+describe("Given a generalError middleware", () => {
+  const req = {};
+  const res: Partial<Response> = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  };
+  const next = jest.fn();
+
+  describe("When it receives an error with status code 404 and the error message 'Endpoint not found'", () => {
+    const error = new CustomError("Endpoint not found", 404);
+
+    beforeEach(() => {
+      generalError(
+        error,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+    });
+
+    test("Then it should call the response method with the message 'Endpoint not found'", () => {
+      const expectedMessage = "Endpoint not found";
+
+      expect(res.json).toHaveBeenCalledWith({ message: expectedMessage });
+    });
+
+    test("Then it should call the status method with status code 404", () => {
+      const expectedStatus = 404;
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+    });
+  });
+
+  describe("When it receives an error without status code", () => {
+    const error = new Error();
+
+    beforeEach(() => {
+      generalError(
+        error as CustomError,
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+    });
+    test("Then it should call the response method with the message 'Internal server error'", () => {
+      const expectedMessage = "Internal server error";
+
+      expect(res.json).toHaveBeenCalledWith({ message: expectedMessage });
+    });
+
+    test("Then it should call the status method with status code 500", () => {
+      const expectedStatus = 500;
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
     });
   });
 });
