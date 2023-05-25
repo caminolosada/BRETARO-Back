66e4ec3e0e628647ecc1832a2d9a874f32eb4424
Camino Losada <camino.losada.alonso@gmail.com>
2023-05-25 12:02:02 +0200

    Start server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import "./loadEnvironment.js";
 import createDebug from "debug";
 import chalk from "chalk";
+import { app } from "./server/index.js";
 
 const debug = createDebug("bretaro-api:root");
 
 debug(chalk.greenBright("testing debug"));
+
+const port = process.env.PORT ?? 4000;
+
+app.listen(port, () => {
+  debug(chalk.greenBright(`Connected to port ${port}`));
+});
