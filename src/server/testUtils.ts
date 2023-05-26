875de8044ad01638764cb5bb680aa201cf90011b
Camino Losada <camino.losada.alonso@gmail.com>
2023-05-26 10:14:27 +0200

    Add test cases for loginUser controller

--- src/server/testUtils.ts
+++ src/server/testUtils.ts
@@ -1,6 +1,17 @@
+import { Types } from "mongoose";
+import { type UserData } from "../types/types";
+
 export interface CustomResponse {
   status: number;
   body: {
     mesage: string;
   };
 }
+
+export const mockedUser: UserData = {
+  _id: new Types.ObjectId().toString(),
+  username: "camino",
+  password: "camino1234",
+};
+
+export const mockedToken = "lskdnflskdnf";
